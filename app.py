99373aee3800e87e7ebd5b4f26f108869bd25d05
Banto21 <tomasz.banasiak21@gmail.com>
2023-05-14 17:24:09 +0200

    kolejny update app.py

--- app.py
+++ app.py
@@ -2,9 +2,13 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
  """message"""
  return '<h1>Hello WSB! Greetings from Flask!</h1>'
+
+
 if __name__ == "__main__":
  app.run(debug=True)
